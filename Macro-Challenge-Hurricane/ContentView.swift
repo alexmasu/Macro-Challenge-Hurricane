6d5dee50e98af0ca63840a6dd7e04e6bf707902e
alexmasu <alexmasu99@gmail.com>
2022-05-13 09:57:20 +0200

    Initial Commit

---
+++ Macro-Challenge-Hurricane/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Macro-Challenge-Hurricane
+//
+//  Created by Alessandro Masullo on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macro-Challenge-Hurricane/Macro_Challenge_HurricaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_Challenge_HurricaneApp.swift
+//  Macro-Challenge-Hurricane
+//
+//  Created by Alessandro Masullo on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Macro_Challenge_HurricaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
