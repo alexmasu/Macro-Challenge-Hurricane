aa067123c406c2bded5ca27069e0cdd48ae0e06a
GiovanniTeresi <giovanni98.teresi@gmail.com>
2022-05-20 13:00:37 +0200

    creata la cartella del rhythm game

--- Macro-Challenge-Hurricane/ButtonPlacement.swift
+++ Macro-Challenge-Hurricane/ButtonPlacement.swift
@@ -45,7 +45,42 @@ struct ButtonPlacement: View {
                     .position(x: UIScreen.main.bounds.width*0.88, y: UIScreen.main.bounds.height*0.1)
                     .foregroundColor(.gray)
             }
+            
+            Circle()
+                .frame(width: 450, height: 450)
+                .foregroundColor(.red)
+                .opacity(0.5)
+                .position(x: UIScreen.main.bounds.width, y: UIScreen.main.bounds.height*0.05)
+            
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 65, height: 65)
+                    .position(x: UIScreen.main.bounds.width*0.57, y: UIScreen.main.bounds.height*0.085)
+                    .foregroundColor(.brown)
+            }
+            
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 65, height: 65)
+                    .position(x: UIScreen.main.bounds.width*0.685, y: UIScreen.main.bounds.height*0.175)
+                    .foregroundColor(.brown)
+            }
+            
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 65, height: 65)
+                    .position(x: UIScreen.main.bounds.width*0.88, y: UIScreen.main.bounds.height*0.24)
+                    .foregroundColor(.brown)
+            }
+            
         }.ignoresSafeArea()
+        
     }
 }
 
---
+++ Macro-Challenge-Hurricane/Quiz/RhytmGame/RhytmGame.swift
@@ -0,0 +1,11 @@
+//
+//  RhytmGame.swift
+//  Macro-Challenge-Hurricane
+//
+//  Created by Giovanni Teresi on 20/05/22.
+//
+
+import Foundation
+import SpriteKit
+
+
