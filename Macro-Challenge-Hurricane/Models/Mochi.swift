aaaea3770c75a6eedf63a21fba1b9325244c3155
Marco <marco.scaglione998@gmail.com>
2022-05-20 12:03:12 +0200

    koi

---
+++ Macro-Challenge-Hurricane/Models/Mochi.swift
@@ -0,0 +1,47 @@
+//
+//  Mochi.swift
+//  Macro-Challenge-Hurricane
+//
+//  Created by Marco Scaglione on 20/05/22.
+//
+
+import Foundation
+class Mochi{
+    var maxHunger : Int
+    var maxThirst : Int
+    var maxCleanlyness : Int
+    var maxHealth : Int
+    var maxEnergy : Int
+    var maxHappiness : Int
+    var hunger : Int
+    var thirst: Int
+    var cleanlyness: Int
+    var health: Int
+    var energy: Int
+    var happiness: Int
+    var ageType: Int
+    var followers: Int
+    var money: Int
+    var bits : Int
+    
+    init (MaxHunger : Int, MaxThirst : Int, MaxCleanlyness : Int, MaxHealth : Int, MaxEnergy : Int, MaxHappyness : Int, Hunger : Int, Thirst : Int, Cleanlyness :Int, Health : Int, Energy : Int, Happiness : Int, AgeType : Int, Followers : Int, Money : Int, Bits :Int){
+        
+        maxHunger = MaxHunger
+        maxThirst = MaxThirst
+        maxCleanlyness = MaxCleanlyness
+        maxHealth = MaxHealth
+        maxEnergy = MaxEnergy
+        maxHappiness = MaxHappyness
+        hunger = Hunger
+        thirst = Thirst
+        cleanlyness = Cleanlyness
+        health = Health
+        energy = Energy
+        happiness = Happiness
+        ageType = AgeType
+        followers = Followers
+        money = Money
+        bits = Bits
+        
+    }
+}
