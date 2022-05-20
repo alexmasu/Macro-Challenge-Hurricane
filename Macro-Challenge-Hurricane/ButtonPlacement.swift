7c8328a1a3b8efb67157c19b6a04ef007bf1190d
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-05-20 12:08:30 +0200

    Aggiustata la posizione dei 4 bottoni alle estremità.

--- Macro-Challenge-Hurricane/ButtonPlacement.swift
+++ Macro-Challenge-Hurricane/ButtonPlacement.swift
@@ -9,42 +9,48 @@ import SwiftUI
 
 struct ButtonPlacement: View {
     var body: some View{
-        Button(){}
-        label :
-        {
-            Rectangle()
-                .frame(width: 69, height: 69)
-                .position(x: UIScreen.main.bounds.width*0.13, y: UIScreen.main.bounds.height*0.05)
-                .foregroundColor(.blue)
+        ZStack{
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 69, height: 69)
+                    .position(x: UIScreen.main.bounds.width*0.12, y: UIScreen.main.bounds.height*0.05)
+                    .foregroundColor(.blue)
+            }
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 69, height: 69)
+                    .position(x: UIScreen.main.bounds.width*0.12, y: UIScreen.main.bounds.height*0.85)
+                    .foregroundColor(.green)
+            }
+            
+            
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 69, height: 69)
+                    .position(x: UIScreen.main.bounds.width*0.88, y: UIScreen.main.bounds.height*0.85)
+                    .foregroundColor(.black)
+            }
+            
+            Button(){}
+            label :
+            {
+                Rectangle()
+                    .frame(width: 69, height: 69)
+                    .position(x: UIScreen.main.bounds.width*0.88, y: UIScreen.main.bounds.height*0.05)
+                    .foregroundColor(.gray)
+            }
         }
-        Button(){}
-        label :
-        {
-            Rectangle()
-                .frame(width: 69, height: 69)
-                .position(x: UIScreen.main.bounds.width*0.13, y: UIScreen.main.bounds.height*0.4)
-                .foregroundColor(.green)
-        }
-        
-        
-        Button(){}
-        label :
-        {
-            Rectangle()
-                .frame(width: 69, height: 69)
-                .position(x: UIScreen.main.bounds.width*0.87, y: UIScreen.main.bounds.height*0.40)
-                .foregroundColor(.black)
-        }
-        
-        Button(){}
-        label :
-        {
-            Rectangle()
-                .frame(width: 69, height: 69)
-//                .position(x: UIScreen.main.bounds.width*0.87, y: UIScreen.main.bounds.height*0.10)
-                .position(x: 0, y: 0)
-                .foregroundColor(.gray)
-        }
-        
+    }
+}
+
+struct ButtonPlacement_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonPlacement()
     }
 }
