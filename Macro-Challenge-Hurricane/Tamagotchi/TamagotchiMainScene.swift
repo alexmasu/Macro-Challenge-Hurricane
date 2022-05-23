a5366e06e66bf889e27e803e3a3b242f1a91e385
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-05-23 16:19:49 +0200

    Added the 4 main buttons in SpriteKit with Tappable Settings with 3 options.

--- Macro-Challenge-Hurricane/Tamagotchi/TamagotchiMainScene.swift
+++ Macro-Challenge-Hurricane/Tamagotchi/TamagotchiMainScene.swift
@@ -12,12 +12,26 @@ class TamagotchiMainScene :  SKScene {
     
     let square = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.2)
     let setting = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
-    
+    let inventory = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+    let shop = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+    let customization = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+    let audiobutton = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+    let twitchbutton = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+    let languagebutton = SKShapeNode(rectOf: CGSize(width: UIScreen.main.bounds.width * 0.15, height: UIScreen.main.bounds.width * 0.15))
+//    let cerchio = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.30)
+    var settingsOn = false
     
     func colorChange(cosino : SKShapeNode) {
         cosino.fillColor = UIColor.blue
         cosino.strokeColor = UIColor.blue
     }
+    
+    func spawnButtons(sett : SKShapeNode){
+        self.setting.addChild(audiobutton)
+        self.setting.addChild(twitchbutton)
+        self.setting.addChild(languagebutton)
+    }
+    
     
     override func didMove(to view: SKView) {
         square.fillColor = UIColor.red
@@ -26,10 +40,48 @@ class TamagotchiMainScene :  SKScene {
         square.name = "ometto"
         addChild(square)
         
+        audiobutton.fillColor = UIColor.green
+        audiobutton.strokeColor = UIColor.red
+        audiobutton.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.height * 0.20, y: frame.maxY - UIScreen.main.bounds.width * 0.185)
+        audiobutton.name = "audio"
+        
+        twitchbutton.fillColor = UIColor.green
+        twitchbutton.strokeColor = UIColor.red
+        twitchbutton.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.height * 0.16, y: frame.maxY - UIScreen.main.bounds.width * 0.365)
+        twitchbutton.name = "twitch"
+        
+        languagebutton.fillColor = UIColor.green
+        languagebutton.strokeColor = UIColor.red
+        languagebutton.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.height * 0.07, y: frame.maxY - UIScreen.main.bounds.width * 0.465)
+        languagebutton.name = "language"
+        
+//        cerchio.fillColor = UIColor.white
         setting.fillColor = UIColor.blue
         setting.strokeColor = UIColor.white
-        setting.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.width * 0.15 , y: frame.maxY - UIScreen.main.bounds.width * 0.15)
+        setting.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.width * 0.15 , y: frame.maxY - UIScreen.main.bounds.width * 0.20)
+        setting.name = "setting"
+//        cerchio.position = setting.position
+//        addChild(cerchio)
         addChild(setting)
+        
+        
+        inventory.fillColor = UIColor.blue
+        inventory.strokeColor = UIColor.white
+        inventory.position = CGPoint(x: frame.maxX - UIScreen.main.bounds.width * 0.15 , y: frame.minY + UIScreen.main.bounds.width * 0.15)
+        inventory.name = "inventory"
+        addChild(inventory)
+        
+        shop.fillColor = UIColor.blue
+        shop.strokeColor = UIColor.white
+        shop.position = CGPoint(x: frame.minX + UIScreen.main.bounds.width * 0.15 , y: frame.minY + UIScreen.main.bounds.width * 0.15)
+        shop.name = "shop"
+        addChild(shop)
+        
+        customization.fillColor = UIColor.blue
+        customization.strokeColor = UIColor.white
+        customization.position = CGPoint(x: frame.minX + UIScreen.main.bounds.width * 0.15 , y: frame.maxY - UIScreen.main.bounds.width * 0.20)
+        customization.name = "customization"
+        addChild(customization)
     }
     
     
@@ -45,6 +97,18 @@ class TamagotchiMainScene :  SKScene {
             for node in touchedNodes {
                 if node.name == "ometto" {
                     colorChange(cosino: (node as! SKShapeNode ))
+                }
+                if node.name == "setting" && settingsOn == false {
+                    
+                    addChild(audiobutton)
+                    addChild(twitchbutton)
+                    addChild(languagebutton)
+                    settingsOn = true
+                    
+                }
+                 else if node.name == "setting" && settingsOn == true {
+                    removeChildren(in: [audiobutton, twitchbutton, languagebutton])
+                    settingsOn = false
                 }
             }
         }
