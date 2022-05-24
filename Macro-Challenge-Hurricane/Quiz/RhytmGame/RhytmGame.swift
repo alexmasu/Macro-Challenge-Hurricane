6db72a2e532f3953431ee9bfc8143a8d15c3f1db
GiovanniTeresi <giovanni98.teresi@gmail.com>
2022-05-24 10:41:36 +0200

    modifiche di alcuni elementi ed aggiunta la view per il rhythm game

--- Macro-Challenge-Hurricane/Quiz/RhytmGame/RhytmGame.swift
+++ Macro-Challenge-Hurricane/Quiz/RhytmGame/RhytmGame.swift
@@ -11,7 +11,41 @@ import SpriteKit
 class RhytmGame: SKScene {
     
     
+    let upsx = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.1)
+    let updx = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.1)
+    let downsx = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.1)
+    let downdx = SKShapeNode(circleOfRadius: UIScreen.main.bounds.width * 0.1)
+    
     override func didMove(to view: SKView) {
+        
+        upsx.name = "upsx"
+        upsx.position = CGPoint(x: frame.maxX * 0.25, y: frame.maxY * 0.65)
+        upsx.fillColor = UIColor.blue
+        upsx.strokeColor = UIColor.white
+        upsx.lineWidth = CGFloat(UIScreen.main.bounds.width * 0.01)
+        addChild(upsx)
+        
+        
+        updx.name = "updx"
+        updx.position = CGPoint(x: frame.maxX * 0.75, y: frame.maxY * 0.65)
+        updx.fillColor = UIColor.blue
+        updx.strokeColor = UIColor.white
+        updx.lineWidth = CGFloat(UIScreen.main.bounds.width * 0.01)
+        addChild(updx)
+        
+        downsx.name = "downsx"
+        downsx.position = CGPoint(x: frame.maxX * 0.25, y: frame.maxY * 0.25)
+        downsx.fillColor = UIColor.blue
+        downsx.strokeColor = UIColor.white
+        downsx.lineWidth = CGFloat(UIScreen.main.bounds.width * 0.01)
+        addChild(downsx)
+        
+        downdx.name = "downdx"
+        downdx.position = CGPoint(x: frame.maxX * 0.75, y: frame.maxY * 0.25)
+        downdx.fillColor = UIColor.blue
+        downdx.strokeColor = UIColor.white
+        downdx.lineWidth = CGFloat(UIScreen.main.bounds.width * 0.01)
+        addChild(downdx)
         
     }
     
