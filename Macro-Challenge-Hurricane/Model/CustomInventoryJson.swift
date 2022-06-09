e8588a8bd0a3dc90f6ebffe5bdd20d9a6e52372e
alexmasu <alexmasu99@gmail.com>
2022-06-09 13:56:53 +0200

    default value

--- Macro-Challenge-Hurricane/Model/CustomInventoryJson.swift
+++ Macro-Challenge-Hurricane/Model/CustomInventoryJson.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 struct CustomInventoryJson: Codable{
-    var i: [Int] = []
+    var i: [Int] = [0,1,2,3,4,5,6,7,8,9,10,11]
 }
--- Macro-Challenge-Hurricane/Model/StreamingInventoryJson.swift
+++ Macro-Challenge-Hurricane/Model/StreamingInventoryJson.swift
@@ -9,5 +9,5 @@ import Foundation
 
 
 struct StreamingInventoryJson: Codable{
-    var i: [Int] = []
+    var i: [Int] = [0,1,2,3,4,5,6,7,8,9,10,11]
 }
